61d616bfb9697775a1a74ee7196700444d64028d
Shane D Wilson <me@shanedwilson.com>
2018-12-17 15:38:19 -0600

    chapter6 done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import StorePicker from './components/StorePicker';
+import App from './components/App';
+
 
 import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
