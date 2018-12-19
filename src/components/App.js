671ef4c124ac9e20ac24c87308a042221ba93359
Shane D Wilson <me@shanedwilson.com>
2018-12-19 14:27:08 -0600

    finished chapter16

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,12 @@ import Fish from './Fish';
 
     loadSampleFishes = () => {
       this.setState({ fishes: sampleFishes });
+    };
+
+    addToOrder = (key) => {
+      const order = {...this.state.order};
+      order[key] = order[key] + 1 || 1;
+      this.setState({order});
     }
 
    render() {
@@ -28,7 +34,12 @@ import Fish from './Fish';
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key =>
+            <Fish
+              key={key}
+              index={key}
+              details={this.state.fishes[key]}
+              addToOrder={this.addToOrder} />)}
           </ul>
         </div>
           <Order />
