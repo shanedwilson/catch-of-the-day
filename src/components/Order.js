8eca7d675215744658dc12a3dbf8a8bfcbe9ea2c
Shane D Wilson <me@shanedwilson.com>
2018-12-21 12:26:44 -0600

    finished chapter19

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,8 @@ class Order extends React.Component {
   renderOrder = (key) => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    if(!fish) return null;
     if(!isAvailable) {
       return <li key={key}>
         Sorry {fish ? fish.name : 'fish'} is no longer available.
@@ -35,7 +36,7 @@ class Order extends React.Component {
       <ul className="order">
         {orderIds.map(this.renderOrder)}
       </ul>
-      <div class="total">
+      <div className="total">
         <strong>{formatPrice(total)}</strong>
       </div>
      </div>
