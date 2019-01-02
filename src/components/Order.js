eac5f2581d8d86174e36989230718f65a0ae3b1f
Shane D Wilson <me@shanedwilson.com>
2019-01-02 15:44:26 -0600

    finished chapter21

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,14 +7,16 @@ class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
+    const transitionOptions = {
+      classNames: "order",
+      key,
+      timeout: { enter:500, exit:500 }
+    }
+
     if(!fish) return null;
     if(!isAvailable) {
       return (
-        <CSSTransition
-          classNames="order"
-          key={key}
-          timeout={{enter:250, exit:250}}
-         >
+        <CSSTransition {...transitionOptions}>
           <li key={key}>
             Sorry {fish ? fish.name : 'fish'} is no longer available.
           </li>
@@ -22,15 +24,11 @@ class Order extends React.Component {
       );
     }
     return (
-    <CSSTransition
-      classNames="order"
-      key={key}
-      timeout={{enter:250, exit:250}}
-    >
+    <CSSTransition {...transitionOptions}>
       <li key={key}>
         <span>
           <TransitionGroup component="span" className="count">
-            <CSSTransition classNames="count" key={count} timeout={{enter:250, exit:250}}>
+            <CSSTransition classNames="count" key={count} timeout={{enter:500, exit:500}}>
               <span>{count}</span>
             </CSSTransition>
           </TransitionGroup>
